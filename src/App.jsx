32f1f35361b6c79766eabb7829ab61056834cc7d
JoanCaba <38809439+JoanCaba@users.noreply.github.com>
2023-07-14 14:34:26 +0200

    comp: Footer

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import './App.css'
 import Home from './pages/Home/Home'
 import Header from './components/Header/Header'
 import About from './pages/About/About'
+import Footer from './components/Footer/Footer'
 
 function App() {
 
@@ -10,9 +11,10 @@ function App() {
     <>
       <Header />
       <Routes>
-        <Route path="/" element={<Home></Home>}> </Route>
-        <Route path="/about" element={<About></About>}> </Route>
+        <Route path="/" element={<Home />}> </Route>
+        <Route path="/about" element={<About />}> </Route>
       </Routes >
+      <Footer />
     </>
   )
 }
--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,8 +1,10 @@
-import React from 'react'
-
+import { useTranslation } from 'react-i18next'
+import './Footer.css'
+import { fullName } from '/src/data/personal_data.json'
 const Footer = () => {
+  const { t } = useTranslation();
   return (
-    <div>Footer</div>
+    <footer><p>{t('madeWith')} {fullName}</p></footer>
   )
 }
 
